8b4ee42bbd1c54fdf51385370eec397026bfda9e
JF21 <Justinfu21@gmail.com>
2015-05-25 16:49:18 -0400

    Menu Bar

---
+++ app.js
@@ -0,0 +1,37 @@
+var main = function() {
+  /* Push the body and the nav over by 285px over */
+  var retracted = true;
+  
+  $('.icon-menu').mouseenter(function() {
+    if(retracted){
+        $('.menu').animate({
+          top: "0px"
+        }, 200);
+    
+        $('body').animate({
+          top: $('.menu').height()
+        }, 200);
+        
+        retracted = false;
+    }
+  });
+
+  /* Then push them back */
+  $('.menu').mouseleave(function() {
+    $('.menu').animate({
+      top: -$('.menu').height()
+    }, 500);
+
+    $('body').animate({
+      top: "0px"
+    }, 500);
+    
+    setTimeout(function(){
+    retracted = true;
+}, 500);
+   
+  });
+};
+
+
+$(document).ready(main);
